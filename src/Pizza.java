930ef317587cded9544d89ecb5c47828077dbd83
JivoJab <nana0001@stud.kea.dk>
2024-10-29 10:55:26 +0100

    Fourth commit

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,21 +1,19 @@
-public class Pizza
-{
+public class Pizza {
     /** Standardtoppings used for all pizzas **/
     protected String pizzaDefaultToppings;
 
 
     /** Constructor that initializes pizzaDefaultToppings **/
-    public Pizza(String defaultToppings)
-    {
+    public Pizza(String defaultToppings) {
         this.pizzaDefaultToppings = defaultToppings;
     }
 }
 
 /** Subclass representing a menu card of pizzas**/
-public class MenuCard extends Pizza {
+class MenuCard extends Pizza {
 
     /** Array containing all pizzas in the menu card **/
-    private PizzaItem[] pizzaItems;
+    private final PizzaItem[] pizzaItems;
 
 
     /** Constructor for MenuCard that receives default toppings and an array of pizzas **/
@@ -26,44 +24,55 @@ public class MenuCard extends Pizza {
     }
 
     /** Inner class that describes individual pizzas in the menu card **/
-    public static class PizzaItem
-    {
+    public static class PizzaItem {
 
-        /** Attributes **/
+        /**
+         * Attributes
+         **/
         private String pizzaName;
         private int pizzaNumber;
         private double pizzaPrice;
         private String pizzaToppings;
-    }
-
-    /** Constructor that initializes the name, number, price, and toppings for each pizza **/
-        public Pizza(String name, int number, double price, String toppings)
-    {
-        this.pizzaName = name;
-        this.pizzaNumber = number;
-        this.pizzaPrice = price;
-        this.pizzaToppings = toppings;
-    }
-
-    /** toString method that returns a readable description of the pizza **/
-    public String toString()
-    {
-        return "Pizza " + pizzaNumber + ": " + pizzaName +
-                " (" + pizzaToppings + ", " + pizzaDefaultToppings + ") - " + pizzaPrice  + " DKK";
-    }
 
 
-    /** Method to print all the pizzas from the MenuCard **/
-    public void printMenu()
-    {
-        for(PizzaItem pizza : pizzaItems)
-        {
-            System.out.println(pizza);
+        /**
+         * Constructor that initializes the name, number, price, and toppings for each pizza
+         **/
+        public PizzaItem(String name, int number, double price, String toppings) {
+            this.pizzaName = name;
+            this.pizzaNumber = number;
+            this.pizzaPrice = price;
+            this.pizzaToppings = toppings;
         }
 
+        /**
+         * toString method that returns a readable description of the pizza
+         **/
+        @Override
+        public String toString() {
+            return "Pizza " + pizzaNumber + ": " + pizzaName +
+                    " (" + pizzaToppings + ") - " + pizzaPrice + " DKK";
+        }
     }
 
+        /** Method to print all the pizzas from the MenuCard **/
+        public void printMenu() {
+            for (PizzaItem pizza : pizzaItems) {
+                System.out.println(pizza);
+            }
+
+        }
 
 }
 
 
+/**  summary of the key components and how they work together:
+  1. Pizza Class: Holds pizzaDefaultToppings, which is inherited by MenuCard.
+
+  2. MenuCard Class:
+ Contains an array of PizzaItem objects representing each pizza on the menu.
+ Has a printMenu method to print all pizzas in the pizzaItems array.
+
+ 3. PizzaItem Inner Class: Represents individual pizzas with specific attributes
+ and provides a toString method for easy display.
+ **/
